18df3a6b36a0553a954526416123e5e929236e85
Zainab Mustufa <63682505+zabi66@users.noreply.github.com>
2023-01-18 17:25:53 +0000

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,8 @@
+// var x = document.getElementById("head");
+//  x.style.color = "#19afe7";
+
+// function changeColor(){
+//  x.style.color = x.style.color == "#19afe7"?"red":"#19afe7";
+// }
+
+// window.setInterval(changeColor,3000);
